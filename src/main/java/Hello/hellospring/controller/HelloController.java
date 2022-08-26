c3fca20b392bae83ede79e0c728819fdec65f9b7
yunjs24 <yunjs24@gmail.com>
2022-08-26 12:42:58 +0900

    hello-api test

--- src/main/java/Hello/hellospring/controller/HelloController.java
+++ src/main/java/Hello/hellospring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package Hello.hellospring.controller;
 
+import Hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,39 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    /**
+     * @ResponseBody
+     * HTTP의 Body에 문자 내용을 직접 반환함
+     * viewResolver 대신 HttpMessageConverter가 동작
+     * 기본 문자처리: StringHttpMessageConverter
+     * 기본 객체처리: MappingJackson2HttpMessageConverter
+     * byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+     * */
+
     @GetMapping("hello-string")
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
         return "hello" + name; // "hello String"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
